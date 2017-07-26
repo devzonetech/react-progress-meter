3cca7fbd4448e399ab313cb2c7fb33c84c26704b
Zdravko Shishmanov <zdrsoft@gmail.com>
2017-07-26 20:37:26 +0300

    ReactProgressMeter prepared for publishing on npm as module

---
+++ src/ReactProgressMeter.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "!style-loader!css-loader!../css/styles.css";
+import "!style-loader!css-loader!../css/bar.styles.css";
+
+class ReactProgressMeter extends React.Component {
+    
+    constructor() {
+        super();
+    }
+    
+    render() {
+        
+        var progressClasses = "bar bar-" + this.props.currentProgress + " " + this.props.color;
+        
+        
+        return (
+            <div className={this.props.show ? "chart showProgress" : "chart hideProgress"}>
+                <div className={progressClasses}>
+                    <div className="face top">
+                        <div className="growing-bar"></div>
+                    </div>
+                    <div className="face side-0">
+                        <div className="growing-bar"></div>
+                    </div>
+                    <div className="face floor">
+                        <div className="growing-bar">{this.props.showPercent ? this.props.currentProgress + '%' : '' }</div>
+                    </div>
+                    <div className="face side-a"></div>
+                    <div className="face side-b"></div>
+                    <div className="face side-1">
+                        <div className="growing-bar"></div>
+                    </div>
+                </div>
+            </div>);    
+    }
+}
+
+ReactProgressMeter.propTypes = {
+    currentProgress: PropTypes.number,
+    show: PropTypes.bool,
+    showPercent: PropTypes.bool,
+    color: PropTypes.string,
+};
+
+ReactProgressMeter.defaultProps = {
+    currentProgress: 75,
+    show: true,
+    showPercent: false,
+    color: "cyan"
+};
+
+export default ReactProgressMeter;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+/*
+ * This webpack config produces two js files: one is npm module file ReactProgressMeter.js, the other one is example app js file index.js in dist directory.
+ * Devzone Tech Ltd.
+ * https://www.devzonetech.com/
+ */
+
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, './src/');
+
+var config = {
+  devtool:'sourcemaps',
+  cache: true,
+  entry: {
+      ReactProgressMeter: APP_DIR + '/ReactProgressMeter.js',
+      index: APP_DIR + '/index.js',
+  },
+  output: {
+    path: BUILD_DIR,
+        filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module : {
+     loaders: [
+            {
+                test: /(\.scss|\.css)$/,
+                loaders: [
+                    require.resolve('style-loader'),
+                    require.resolve('css-loader') + '?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+                    require.resolve('sass-loader') + '?sourceMap'
+                ]
+            },
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: { presets:['es2015', 'stage-0', 'react' ], plugins:[]}
+            },
+        ],
+  },
+   plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                // Drop `console` statements
+                drop_console: true
+            },
+            output: {
+                comments: false,
+            },
+            sourceMap: false,
+            // Don't beautify output (enable for neater output)
+            beautify: false,
+        }),
+        new webpack.LoaderOptionsPlugin({
+          debug: true
+        }),
+    ]
+};
+
+module.exports = config;
